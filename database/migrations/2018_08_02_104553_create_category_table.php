6f5fdf6f2ec449ac0720d95971f835a2eb3735fd
Ganesh Khadka <ganeshkhadka46@gmail.com>
2018-08-02 17:08:45 +0545

    changes in migration

---
+++ database/migrations/2018_08_02_104553_create_category_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCategoryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+
+            $table->string('title',255);
+            $table->string('slug',100)->unique();
+            $table->text('image')->nullable();
+            $table->boolean('status')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
