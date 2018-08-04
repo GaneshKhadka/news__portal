9be7a2093a728f43b9eaed4fbf22fa7139ad4c89
Ganesh Khadka <ganeshkhadka46@gmail.com>
2018-08-04 10:04:52 +0545

    changes in migration .ie created news_table

--- database/migrations/2018_08_02_110645_create_news_table.php
+++ database/migrations/2018_08_02_110645_create_news_table.php
@@ -16,6 +16,14 @@ class CreateNewsTable extends Migration
         Schema::create('news', function (Blueprint $table) {
             $table->increments('id');
             $table->timestamps();
+
+            $table->string('title',255);
+            $table->string('slug',100)->unique();
+            $table->string('writer',100)->nullable();
+            $table->text('short_des')->nullabe();
+            $table->longText('detail_des')->nullable();
+            $table->dateTime('publish_date');
+            $table->boolean('status')->default(0);
         });
     }
 
